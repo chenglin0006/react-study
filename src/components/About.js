43a3d4a157b56f9b0ea617e7c8b9fb46d590f5d2
程林 <chenglin@wuh1701sd04.b-and-qchina.com>
2018-06-08 14:30:29 +0800

    update

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class About extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <div>ABOUT</div>
+            </div>
+        )
+    }
+}
+
+export default About
